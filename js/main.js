46d6b689cc41c1248324028b252287413fbb8ef7
bihanns <bihanns@gmail.com>
2021-08-24 01:37:51 +0800

    update
    update

--- js/main.js
+++ js/main.js
@@ -57,11 +57,11 @@ setInterval(changeWord, 4000);
 
 // popup js menu mobile
 
-function myFunction() {
-  var x = document.getElementById("mobile-nav");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+// function myFunction() {
+//   var x = document.getElementById("mobile-nav");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
